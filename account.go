77b895c55c7e33b8af37bfc34edc8b6e400e59d2
Mathieu Corbin <contact@mcorbin.fr>
2023-05-14 22:23:07 +0200

    Reset password : rename type to get a type

--- account.go
+++ account.go
@@ -4,6 +4,6 @@ type ChangeAccountPasswordInput struct {
 	NewPassword string `json:"new-password" description:"User new password" validate:"required,min=10,max=255"`
 }
 
-type ResetAccountPasswordInput struct {
+type ResetAccountPasswordLinkInput struct {
 	Email string `json:"email" validate:"required"`
 }
